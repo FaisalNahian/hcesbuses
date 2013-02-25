95e850685580280f6a7d18be87e147c0e73b43d6
H. Wade Minter <minter@teamsnap.com>
2013-02-25 15:39:47 -0500

    Better defaults after sign-in/sign-out.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def after_sign_in_path_for(user)
+    trackers_url
+  end
+  
+  def after_sign_out_path_for(user)
+    pages_index_url
+  end
+  
 end
