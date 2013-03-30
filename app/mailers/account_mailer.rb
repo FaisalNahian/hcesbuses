24c8348730013ba04a72ee8d0ed7bca0cf25cd87
H. Wade Minter <minter@teamsnap.com>
2013-03-29 22:56:31 -0400

    Add basic HTML template, initial "You didn't confirm" email.

---
+++ app/mailers/account_mailer.rb
@@ -0,0 +1,24 @@
+class AccountMailer < ActionMailer::Base
+  default from: "info@hcesbuses.com"
+  
+  def unfinished_registration_reminder(user_id)
+    @user = User.find(user_id)
+    @subject = "Reminder about your HCESBuses.com account"
+    @preview = "You haven't completed the steps to register for your HCESBuses.com account"
+    mail(:to => @user.email, :subject => @subject) do |format|
+      format.text
+      format.html
+    end
+  end
+  
+  def new_year_subscription_reminder(user_id)
+    @user = User.find(user_id)
+    @subject = "It's a new school year at HCESBuses.com!"
+    @preview = "Be sure that you are correctly set up to watch your buses for this new school year!"
+    mail(:to => @user.email, :subject => @subject) do |format|
+      format.text
+      format.html
+    end
+  end
+  
+end
